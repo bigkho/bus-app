120cdc4179bca50a40a712dfbafabf0b71c24944
Mihad Khondker <mihadkho2019@gmail.com>
2022-08-23 00:53:14 -0400

    adjusted backend GET call

--- backend/routes/journies.js
+++ backend/routes/journies.js
@@ -1,7 +1,54 @@
 const router = require('express').Router();
 let Journey = require('../journey');
+const axios = require("axios");
+
+async function doGetRequest() {
+    await axios.get('http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=56cb8d03-789b-4e75-81d7-bc85d9f7ffc6')
+        .then(res => {
+            Journey.deleteMany({}).then(function(){
+                monitored = res.data.Siri.ServiceDelivery.VehicleMonitoringDelivery[0].VehicleActivity
+                for(var i=0; i<monitored.length; i++) {
+                    busJourney = monitored[i].MonitoredVehicleJourney
+                    bus = busJourney.PublishedLineName
+                    console.log(bus)
+                    lat = busJourney.VehicleLocation.Latitude
+                    long = busJourney.VehicleLocation.Longitude
+                    final = busJourney.DestinationName
+                    dist = "Data unavailable"
+                    stops = "Data unavailable"
+                    expecteds = new Date('December 17, 1995 03:24:00')
+
+                    if (busJourney.hasOwnProperty('MonitoredCall')){
+                        dist = busJourney.MonitoredCall.Extensions.Distances.PresentableDistance
+                        stops = busJourney.MonitoredCall.StopPointName
+                        expecteds = busJourney.MonitoredCall.ExpectedArrivalTime
+                    }
+                    
+                    newJourney = new Journey ({
+                        busID: bus,
+                        location: {
+                            latitude: lat,
+                            longitude: long,
+                        },
+                        finalStop: final,
+                        distance: dist,
+                        stop: stops,
+                        expected: expecteds
+                    })
+
+                    newJourney.save()
+                }
+            }).catch(function(error){
+                console.log(error); // Failure
+            });
+
+            
+        })
+        .catch(err => console.log(err))
+};
 
 router.route('/').get((req,res)=> {
+    doGetRequest()
     Journey.find()
         .then(journies => res.json(journies))
         .catch(err => res.status(400).json('Error: ' + err))
--- backend/server.js
+++ backend/server.js
@@ -22,55 +22,6 @@ connection.once('open', () => {
 const journiesRouter = require('./routes/journies');
 app.use('/journies',journiesRouter);
 
-const Journey = require('./journey')
-
-async function doGetRequest() {
-    await axios.get('http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=56cb8d03-789b-4e75-81d7-bc85d9f7ffc6')
-        .then(res => {
-            Journey.deleteMany({}).then(function(){
-                monitored = res.data.Siri.ServiceDelivery.VehicleMonitoringDelivery[0].VehicleActivity
-                for(var i=0; i<monitored.length; i++) {
-                    busJourney = monitored[i].MonitoredVehicleJourney
-                    console.log(busJourney)
-                    bus = busJourney.PublishedLineName
-                    lat = busJourney.VehicleLocation.Latitude
-                    long = busJourney.VehicleLocation.Longitude
-                    final = busJourney.DestinationName
-                    dist = "Data unavailable"
-                    stops = "Data unavailable"
-                    expecteds = new Date('December 17, 1995 03:24:00')
-
-                    if (busJourney.hasOwnProperty('MonitoredCall')){
-                        dist = busJourney.MonitoredCall.Extensions.Distances.PresentableDistance
-                        stops = busJourney.MonitoredCall.StopPointName
-                        expecteds = busJourney.MonitoredCall.ExpectedArrivalTime
-                    }
-                    
-                    newJourney = new Journey ({
-                        busID: bus,
-                        location: {
-                            latitude: lat,
-                            longitude: long,
-                        },
-                        finalStop: final,
-                        distance: dist,
-                        stop: stops,
-                        expected: expecteds
-                    })
-
-                    newJourney.save()
-                }
-            }).catch(function(error){
-                console.log(error); // Failure
-            });
-
-            
-        })
-        .catch(err => console.log(err))
-};
-
-doGetRequest()
-
 app.listen(port, () => {
     console.log(`Server on port: ${port}`);
 });
